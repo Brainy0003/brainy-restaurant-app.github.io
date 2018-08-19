1426ef11eefeb1796f9fc43aac66233e0239d999
Brainy0003 <aprecious0003@gmail.com>
2018-08-19 09:52:27 +0100

    Progress: 70% done with the project, finally implemented responsiveness in the restaurant.html and index.html pages and implemented service worker and pages are now been cached, application now works offline..

--- sw.js
+++ sw.js
@@ -12,23 +12,26 @@ self.addEventListener('install', event => {
    caches.open(staticCacheName).then(cache => {
 
      return cache.addAll([
-          'index.html',
-          'restaurant.html',
-          'img/1.jpg',
-          'img/2.jpg',
-          'img/3.jpg',
-          'img/4.jpg',
-          'img/5.jpg',
-          'img/6.jpg',
-          'img/7.jpg',
-          'img/8.jpg',
-          'img/9.jpg',
-          'img/10.jpg',
-          'js/dbhelper.js',
-          'js/main.js',
-          'js/restaurant_info.js',
-          'data/restaurant.json',
-          'css/styles.css',
+        /*
+        'index.html',
+        'restaurant.html',
+        'img/1.jpg',
+        'img/2.jpg',
+        'img/3.jpg',
+        'img/4.jpg',
+        'img/5.jpg',
+        'img/6.jpg',
+        'img/7.jpg',
+        'img/8.jpg',
+        'img/9.jpg',
+        'img/10.jpg',
+        'js/dbhelper.js',
+        'js/main.js',
+        'js/restaurant_info.js',
+        'data/restaurant.json',
+        'css/styles.css',
+        */
+
      ]);
    })
  );
@@ -46,7 +49,6 @@ self.addEventListener('activate', event => {
 });
 
 
-
 //fetch cache
 
 self.addEventListener('fetch', event => {
@@ -59,8 +61,8 @@ self.addEventListener('fetch', event => {
 
     .then(cache => {
 
-      cache.put(event.request, response.clone());
-
+      cache.put(event.request, response.clone()); 
+      //, response.clone()
       return response;
 
     })).catch(event => {
